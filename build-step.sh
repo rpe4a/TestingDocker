47005bdf71211f9a1b123fdbb9ea68e76ef5e4c7
rpe4a <zapman2la@gmail.com>
2020-04-22 15:17:36 +0500

    скрипт для создания шага в jenkins

---
+++ build-step.sh
@@ -0,0 +1,29 @@
+# Аргументы для Compose, определяемые по умолчанию
+COMPOSE_ARGS=" -f docker-compose-jenkins.yml -p jenkins "
+# Необходимо остановить и удалить все старые контейнеры
+sudo docker-compose $COMPOSE_ARGS stop
+sudo docker-compose $COMPOSE_ARGS rm --force -v
+# Создание (сборка) системы
+sudo docker-compose $COMPOSE_ARGS build --no-cache
+sudo docker-compose $COMPOSE_ARGS up -d
+# Выполнение модульного тестирования
+sudo docker-compose $COMPOSE_ARGS run --no-deps --rm -e ENV=UNIT identidock
+ERR=$? #код завершения последней операции
+
+# Выполнение тестирования системы в целом, если модульное тестирование завершилось успешно
+if [ $ERR -eq 0 ]; then
+    IP=$(
+        sudo docker inspect -f {{.NetworkSettings.IPAddress}}
+        jenkins_identidock_1
+    )
+    CODE=$(curl -sL -w "%{http_code}" $IP:9090/monster/bla -o /dev/null) || true
+    #вывод в файл /dev/null - говорит о том что нам не важен вывод операции и мы грубо говря хотим его удалить
+    if [ $CODE -ne 200 ]; then
+        echo "Site returned " $CODE
+        ERR=1
+    fi
+fi
+# Останов и удаление системы
+sudo docker-compose $COMPOSE_ARGS stop
+sudo docker-compose $COMPOSE_ARGS rm --force -v
+return $ERR
